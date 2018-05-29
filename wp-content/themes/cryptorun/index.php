7d34033e7d487ef3adcd6a0aa6f68c5d78303635
Travis Gautier <tgautier13@gmail.com>
2018-05-28 23:29:59 -0500

    Getting index.php set up with html boilerplate.

--- wp-content/themes/cryptorun/index.php
+++ wp-content/themes/cryptorun/index.php
@@ -1,5 +1,14 @@
-<?php if ( have_posts()) : ?>                        <!--Retrieve dynamic content from post-->
-	<?php while ( have_posts()) : the_post(); ?>
-		<?php the_content(); ?>
-	<?php endwhile; ?>
-<?php endif; ?>
+<!doctype html>
+<html>
+    <head>
+        <title>CryptoRun Theme</title>
+    </head>
+    <body>
+        <?php if ( have_posts()) : ?>                        <!--Retrieve dynamic content from post-->
+            <?php while ( have_posts()) : the_post(); ?>     <!--While there are posts, go through each post-->
+                <?php the_title(); ?>
+                <?php the_content(); ?>                      <!--Retrieve the title and content of the post-->
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </body>
+</html>
