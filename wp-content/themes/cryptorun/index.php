dee4596924454fd7892666d1aae248c8609fa294
Travis Gautier <tgautier13@gmail.com>
2018-05-28 23:47:16 -0500

    Standardizing index.php

--- wp-content/themes/cryptorun/index.php
+++ wp-content/themes/cryptorun/index.php
@@ -1,7 +1,12 @@
 <!doctype html>
-<html>
+<html <?php language_attributes(); ?>>
     <head>
-        <title>CryptoRun Theme</title>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+	    <?php wp_head(); ?>                                  <!--Important function for integration-->
     </head>
     <body>
         <?php if ( have_posts()) : ?>                        <!--Retrieve dynamic content from post-->
@@ -10,5 +15,6 @@
                 <?php the_content(); ?>                      <!--Retrieve the title and content of the post-->
             <?php endwhile; ?>
         <?php endif; ?>
+        <?php wp_footer(); ?>                                <!--Footer-->
     </body>
 </html>
