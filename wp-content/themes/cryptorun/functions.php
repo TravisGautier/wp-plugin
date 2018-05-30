31c561386acfbcf7032bd549d415702f0c43fa5c
Travis Gautier <tgautier13@gmail.com>
2018-05-30 13:21:23 -0500

    Dynamic menu update

--- wp-content/themes/cryptorun/functions.php
+++ wp-content/themes/cryptorun/functions.php
@@ -3,5 +3,11 @@ function enqueue_cryptorun_styles() {           //loading in style sheets
 	wp_enqueue_style("bootstrap", "//stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css");
 	wp_enqueue_style("cryptorun-style", get_stylesheet_uri());
 }
-add_action("wp_enqueue_scripts", "enqueue_cryptorun_styles")
+add_action("wp_enqueue_scripts", "enqueue_cryptorun_styles");
+
+function cryptorun_setup(){
+	register_nav_menus(array("primary" => __( "Primary Menu", "cryptorun"))); //register a custom primary navigation menu
+	add_theme_support( "title-tag");                                  //add theme support for document title tag
+}
+add_action("after_setup_theme", "cryptorun_setup");
 ?>
--- wp-content/themes/cryptorun/index.php
+++ wp-content/themes/cryptorun/index.php
@@ -5,7 +5,7 @@
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-	    <?php wp_head(); ?>                                      <!--Important function for integration-->
+	    <?php wp_head(); ?>                                         <!--Important function for integration-->
     </head>
     <body>
     <nav class="navbar gradient-bg main-custom-nav">
@@ -21,11 +21,17 @@
             </div><!--.navbar-header-->
             <div class="collapse navbar-collapse" id="bs=example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Link</a></li>
-                    <li><a href="#">Link</a></li>
+                    <?php                                           //Make the menu dynamic
+                        wp_nav_menu( array(
+                                'theme_location' => 'primary',
+                                'menu_class' => 'primary-menu',
+                                'container' => false,
+                                'items_wrap' => '%3%s'
+                        ) );
+                    ?>
                 </ul>
             </div><!--.collapse navbar-collapse-->
-        </div><!--.container-fluid-->
+        </div><!--.container-->
     </nav>
         <div class="jumbotron gradient-bg margin-neg-20">                                  <!--Jumbotron-->
             <div class="container text-center">
